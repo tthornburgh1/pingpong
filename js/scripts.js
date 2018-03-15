92199d34a8233aa12bc292ad32171e28724a8ff9
Tristan Thornburgh <tristan.thornburgh@gmail.com>
2018-03-15 11:48:56 -0500

    producing callbacks for user to see numbers, not quite there

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
+// user interface logic
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+  var userInput = $("#yourNumber").val();
+    event.preventDefault();
+    var result = pingPong(pingpong);
+    $(".showNumber").text();
+    $("#results").show();
+});
+
 // business logic
 var pingPong = function(number) {
+  for (var index = 0; index <= 100000000000000; index += 1)
   if ((number % 3 === 0) && (number % 15 == 0)) {
     return pingpong;
   } else if (number % 3 === 0) {
     return ping;
-  } else {(number % 5 ===0)
+  } else if {(number % 5 ===0)
     return pong;
+  } else {
+    return number;
+  }
 };
-
-// user interface logic
-$(document).ready(function() {
-  $("form#ping-pong").submit(function(event) {
-    event.preventDefault();
-    for (var index = 1; index <= 100000000000000; index += 1)
-    var result = pingPong(pingpong);
-    $("#result").text(result);
-});
