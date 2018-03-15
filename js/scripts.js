eab6c5450089959b701f1afad655ca6dea6ddeb5
Tristan Thornburgh <tristan.thornburgh@gmail.com>
2018-03-15 12:51:20 -0500

    Making headway on the business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
 // user interface logic
-$(document).ready(function() {
-  $("form#ping-pong").submit(function(event) {
-  var userInput = $("#yourNumber").val();
+$(document).ready(function(){
+  $("results form").submit(function(event) {
+    var userInput = parseInt($("input#yourNumber").val());
+      $(".showNumber").text();
+      $("#results").show();
+});
     event.preventDefault();
-    var result = pingPong(pingpong);
-    $(".showNumber").text();
-    $("#results").show();
+  });
 });
 
 // business logic
 var pingPong = function(number) {
-  for (var index = 0; index <= 100000000000000; index += 1)
-  if ((number % 3 === 0) && (number % 15 == 0)) {
-    return pingpong;
-  } else if (number % 3 === 0) {
-    return ping;
-  } else if {(number % 5 ===0)
-    return pong;
-  } else {
-    return number;
-  }
-};
+  var list = []
+  for (var number = 0; number <= pingPongInput; number += 1) {
+    if ((number % 3 === 0) && (number % 15 === 0)) {
+      return "pingpong";
+    } else if (number % 3 === 0) {
+      return "ping";
+    } else if (number % 5 === 0) {
+      return "pong";
+    } else {
+      return number;
+    }
+});
