64c8ece40f40dfa30434c697e22cba423d3bd0df
Tristan Thornburgh <tristan.thornburgh@gmail.com>
2018-03-28 20:07:41 -0500

    More edits to JS, adding console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
-// user interface logic
-$(document).ready(function(){
-  $("results form").submit(function(event) {
-    var userInput = parseInt($("input#yourNumber").val());
-      $(".showNumber").text();
-      $("#results").show();
-});
-    event.preventDefault();
-  });
-});
-
-// business logic
+//business logic
 var pingPong = function(number) {
-  for (var number = 0; number <= pingPongInput; number += 1) {
-    if ((number % 3 === 0) && (number % 15 === 0)) {
-      return "pingpong";
-    } else if (number % 3 === 0) {
-      return "ping";
-    } else if (number % 5 === 0) {
-      return "pong";
-    } else {
-      return number;
-    }
+var userNumberArray = [];
+ for (var index = 1; index <= number; index += 1) {
+   if ((index % 3 === 0) && (index % 15 === 0)) {
+     userNumberArray.push("pingpong");
+   } else if (index % 3 === 0) {
+     userNumberArray.push("ping");
+   } else if (index % 5 === 0) {
+     userNumberArray.push("pong");
+   } else {
+     userNumberArray.push(index);
+   };
+ };
+ console.log(userNumberArray);
+	return userNumberArray;
+};
+
+// user interface logic
+$(document).ready(function() {
+  $("#ping-pong").submit(function(event) {
+    event.preventDefault();
+    var number = $("input#number").val();
+	  var result = pingPong(number);
+    $("#result").text(result);
+  });
 });
